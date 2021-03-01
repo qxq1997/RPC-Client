ca87fbfbb856d801b80637e93c6be1a8b4f64cce
qxq <962850601@qq.com>
2021-03-01 22:47:32 +0800

    修改包名

---
+++ src/main/java/interfaces/ByeService.java
@@ -0,0 +1,8 @@
+package interfaces;
+
+/**
+ * @author by QXQ
+ * @date 2021/2/27.
+ */
+public interface ByeService {
+}
---
+++ src/main/java/interfaces/HelloService.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+import entity.HelloObject;
+
+/**
+ * @author by QXQ
+ * @date 2021/2/27.
+ */
+public interface HelloService {
+
+    String hello(HelloObject object);
+
+}
